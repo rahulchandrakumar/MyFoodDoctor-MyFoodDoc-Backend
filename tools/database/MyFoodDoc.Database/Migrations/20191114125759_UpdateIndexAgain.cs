e8e1debae9648ed3f1ed043c10145500da343e4c
pna <patrick.naumann@appsfactory.de>
2019-11-19 12:43:05 +0100

    adds migrations for database

---
+++ tools/database/MyFoodDoc.Database/Migrations/20191114125759_UpdateIndexAgain.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyFoodDoc.Database.Migrations
+{
+    public partial class UpdateIndexAgain : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AbdonimalGirthHistory_Users_UserId",
+                schema: "User",
+                table: "AbdonimalGirthHistory");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_BloodSugarLevelHistory_Users_UserId",
+                schema: "User",
+                table: "BloodSugarLevelHistory");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_WeightHistory_Users_UserId",
+                schema: "User",
+                table: "WeightHistory");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Meals_Date_Type",
+                schema: "Diary",
+                table: "Meals");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Meals_Date_Type",
+                schema: "Diary",
+                table: "Meals",
+                columns: new[] { "Date", "Type" },
+                unique: true,
+                filter: "Type = 'Snack'");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AbdonimalGirthHistory_Users_UserId",
+                schema: "User",
+                table: "AbdonimalGirthHistory",
+                column: "UserId",
+                principalSchema: "User",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BloodSugarLevelHistory_Users_UserId",
+                schema: "User",
+                table: "BloodSugarLevelHistory",
+                column: "UserId",
+                principalSchema: "User",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WeightHistory_Users_UserId",
+                schema: "User",
+                table: "WeightHistory",
+                column: "UserId",
+                principalSchema: "User",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AbdonimalGirthHistory_Users_UserId",
+                schema: "User",
+                table: "AbdonimalGirthHistory");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_BloodSugarLevelHistory_Users_UserId",
+                schema: "User",
+                table: "BloodSugarLevelHistory");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_WeightHistory_Users_UserId",
+                schema: "User",
+                table: "WeightHistory");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Meals_Date_Type",
+                schema: "Diary",
+                table: "Meals");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Meals_Date_Type",
+                schema: "Diary",
+                table: "Meals",
+                columns: new[] { "Date", "Type" },
+                unique: true,
+                filter: "Type != 'snack'");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AbdonimalGirthHistory_Users_UserId",
+                schema: "User",
+                table: "AbdonimalGirthHistory",
+                column: "UserId",
+                principalSchema: "User",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BloodSugarLevelHistory_Users_UserId",
+                schema: "User",
+                table: "BloodSugarLevelHistory",
+                column: "UserId",
+                principalSchema: "User",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WeightHistory_Users_UserId",
+                schema: "User",
+                table: "WeightHistory",
+                column: "UserId",
+                principalSchema: "User",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
