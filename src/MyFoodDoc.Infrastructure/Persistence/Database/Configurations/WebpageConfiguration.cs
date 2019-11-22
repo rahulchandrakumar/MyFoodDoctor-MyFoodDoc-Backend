b207e18b01ac9a342b28ce2e4f723d84d607aa47
pna <patrick.naumann@appsfactory.de>
2019-11-22 15:40:58 +0100

    MFD-324 - adds entity for customizable web pages

---
+++ src/MyFoodDoc.Infrastructure/Persistence/Database/Configurations/WebpageConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyFoodDoc.Application.Entites;
+
+namespace MyFoodDoc.Infrastructure.Persistence.Database.Configuration
+{
+    public class WebPageConfiguration : IEntityTypeConfiguration<WebPage>
+    {
+        public void Configure(EntityTypeBuilder<WebPage> builder)
+        {
+            builder.ToTable("WebPages", "System");
+            builder.HasKey(o => o.Id);
+            builder.Property(o => o.Url).IsRequired().HasMaxLength(2083);
+            builder.Property(o => o.Title).IsRequired().HasMaxLength(200);
+            builder.Property(o => o.Text).IsRequired();
+            builder.Property(o => o.IsDeletable).IsRequired().HasDefaultValue(true);
+        }
+    }
+}
