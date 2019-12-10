008053dd9ef265c93e9915e663cf7f79703db88f
pna <patrick.naumann@appsfactory.de>
2019-12-10 17:53:20 +0100

    fixes starting problems for api because of seeding file not found

--- src/MyFoodDoc.Infrastructure/Persistence/Database/ApplicationContext.cs
+++ src/MyFoodDoc.Infrastructure/Persistence/Database/ApplicationContext.cs
@@ -19,6 +19,8 @@ namespace MyFoodDoc.Infrastructure.Persistence.Database
 {
     public class ApplicationContext : IdentityDbContext<User>, IApplicationContext
     {
+        public bool WithSeeding { get; set; } = false;
+
         public DbSet<Insurance> Insurances { get; set; }
         public DbSet<Ingredient> Ingredients { get; set; }
         public DbSet<Indication> Indications { get; set; }
@@ -95,204 +97,218 @@ namespace MyFoodDoc.Infrastructure.Persistence.Database
             builder.Entity<IdentityRoleClaim<string>>(entity => entity.ToTable("RoleClaim", "User"));
             builder.Entity<IdentityUserToken<string>>(entity => entity.ToTable("UserToken", "User"));
 
-            builder.Entity<Insurance>().HasData(
-                new Insurance
-                {
-                    Id = 1,
-                    Name = "Aok"
-                },
-                new Insurance
-                {
-                    Id = 2,
-                    Name = "Barmer"
-                },
-                new Insurance
-                {
-                    Id = 3,
-                    Name = "DAK"
-                },
-                new Insurance
-                {
-                    Id = 4,
-                    Name = "hkk"
-                },
-                new Insurance
-                {
-                    Id = 5,
-                    Name = "Techniker"
-                }
-            );
-
-            //,,,diabetes_type_1,
-            builder.Entity<Indication>().HasData(
-                new Indication
-                {
-                    Id = 1,
-                    Key = "hypertension",
-                    Name = "Hypertonie"
-                },
-                new Indication
-                {
-                    Id = 2,
-                    Key = "adipositas",
-                    Name = "Adipositas"
-                },
-                new Indication
-                {
-                    Id = 3,
-                    Key = "gout",
-                    Name = "Gicht"
-                },
-
-                new Indication
-                {
-                    Id = 4,
-                    Key = "diabetes_type_1",
-                    Name = "Diabetes Typ 1"
-                },
-                new Indication
-                {
-                    Id = 5,
-                    Key = "diabetes_type_2",
-                    Name = "Diabetes Typ 2"
-                }
-            );
-
-            builder.Entity<Motivation>().HasData(
-                new Motivation
-                {
-                    Id = 1,
-                    Key = "anti_aging",
-                    Name = "Anti-Aging"
-                },
-                new Motivation
-                {
-                    Id = 2,
-                    Key = "healthier_lifestyle",
-                    Name = "Healthier lifestyle"
-                },
-                new Motivation
-                {
-                    Id = 3,
-                    Key = "reduce_weight",
-                    Name = "Reduce weight"
-                },
-                new Motivation
-                {
-                    Id = 4,
-                    Key = "feel_better",
-                    Name = "Feel better"
-                }
-            );
-
-            builder.Entity<Diet>().HasData(
-                new Diet
-                {
-                    Id = 1,
-                    Key = "vegetarian",
-                    Name = "Vegetarisch"
-                },
-                new Diet
-                {
-                    Id = 2,
-                    Key = "vegan",
-                    Name = "Vegan"
-                },
-                new Diet
-                {
-                    Id = 3,
-                    Key = "interval_fasting",
-                    Name = "Intervallfasten"
-                },
-                new Diet
-                {
-                    Id = 4,
-                    Key = "vegetarian_milk",
-                    Name = "Vegetarisch mit Milch, Ei, Fisch"
-                },
-                new Diet
-                {
-                    Id = 5,
-                    Key = "lactose_free",
-                    Name = "Laktosefrei"
-                },
-                new Diet
-                {
-                    Id = 6,
-                    Key = "pescetarian",
-                    Name = "Vegetarisch mit Fisch"
-                },
-                new Diet
-                {
-                    Id = 7,
-                    Key = "low_fructose",
-                    Name = "Frustosearm"
-                }
-            );
-
-            builder.Entity<Image>().HasData(
-                new Image
-                {
-                    Id = 1,
-                    Url = "https://myfooddoctormockcmsimgs.blob.core.windows.net/images/253f35f4-f3ac-425c-93ff-6edfdb62a12f.jpg"
-                },
-                new Image
-                {
-                    Id = 2,
-                    Url = "https://myfooddoctormockcmsimgs.blob.core.windows.net/images/a5ec0b6f-d3cc-4a52-8283-2d7dba9a560c.jpg"
-                }
-            );
-
-            builder.Entity<LexiconEntry>().HasData(
-                new LexiconEntry
-                {
-                    Id = 1,
-                    TitleShort = "Eiweiß",
-                    TitleLong = "Eiweiß",
-                    ImageId = 1,
-                    Text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
-                },
-                new LexiconEntry
-                {
-                    Id = 2,
-                    TitleShort = "Proteine",
-                    TitleLong = "Proteine",
-                    ImageId = 1,
-                    Text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
-                }
-            );
-
-            var hasher = new PasswordHasher<User>();
-            builder.Entity<User>().HasData(
-                new User
-                {
-                    Id = "3ee857ac-26ee-43d8-8f68-76f1ca7bfa9b",
-                    UserName = "test@appsfactory.de",
-                    NormalizedUserName = "TEST@APPSFACTORY.DE",
-                    Email = "test@appsfactory.de",
-                    NormalizedEmail = "TEST@APPSFACTORY.DE",
-                    EmailConfirmed = true,
-                    PasswordHash = hasher.HashPassword(null, "Wert123+"),
-                    SecurityStamp = string.Empty,
-                    InsuranceId = 1
-                }
-            );
-
-            var resourceName = "bls.csv";
-            using StreamReader reader = new StreamReader(resourceName, Encoding.UTF8);
-            using CsvReader csv = new CsvReader(reader);
-            
-            csv.Configuration.RegisterClassMap<IngredientsMap>();
-            csv.Configuration.BadDataFound = null;
-            csv.Configuration.Delimiter = ",";
-            var ingredients = csv.GetRecords<Ingredient>().Where(x => !x.ExternalKey.EndsWith("00000")).ToArray();
-
-            var id = 1;
-            foreach (var ingredient in ingredients)
+            if (WithSeeding)
             {
-                ingredient.Id = id++;
+
+                builder.Entity<Insurance>().HasData(
+                    new Insurance
+                    {
+                        Id = 1,
+                        Name = "Aok"
+                    },
+                    new Insurance
+                    {
+                        Id = 2,
+                        Name = "Barmer"
+                    },
+                    new Insurance
+                    {
+                        Id = 3,
+                        Name = "DAK"
+                    },
+                    new Insurance
+                    {
+                        Id = 4,
+                        Name = "hkk"
+                    },
+                    new Insurance
+                    {
+                        Id = 5,
+                        Name = "Techniker"
+                    }
+                );
+
+                //,,,diabetes_type_1,
+                builder.Entity<Indication>().HasData(
+                    new Indication
+                    {
+                        Id = 1,
+                        Key = "hypertension",
+                        Name = "Hypertonie"
+                    },
+                    new Indication
+                    {
+                        Id = 2,
+                        Key = "adipositas",
+                        Name = "Adipositas"
+                    },
+                    new Indication
+                    {
+                        Id = 3,
+                        Key = "gout",
+                        Name = "Gicht"
+                    },
+
+                    new Indication
+                    {
+                        Id = 4,
+                        Key = "diabetes_type_1",
+                        Name = "Diabetes Typ 1"
+                    },
+                    new Indication
+                    {
+                        Id = 5,
+                        Key = "diabetes_type_2",
+                        Name = "Diabetes Typ 2"
+                    }
+                );
+
+                builder.Entity<Motivation>().HasData(
+                    new Motivation
+                    {
+                        Id = 1,
+                        Key = "anti_aging",
+                        Name = "Anti-Aging"
+                    },
+                    new Motivation
+                    {
+                        Id = 2,
+                        Key = "healthier_lifestyle",
+                        Name = "Healthier lifestyle"
+                    },
+                    new Motivation
+                    {
+                        Id = 3,
+                        Key = "reduce_weight",
+                        Name = "Reduce weight"
+                    },
+                    new Motivation
+                    {
+                        Id = 4,
+                        Key = "feel_better",
+                        Name = "Feel better"
+                    }
+                );
+
+                builder.Entity<Diet>().HasData(
+                    new Diet
+                    {
+                        Id = 1,
+                        Key = "vegetarian",
+                        Name = "Vegetarisch"
+                    },
+                    new Diet
+                    {
+                        Id = 2,
+                        Key = "vegan",
+                        Name = "Vegan"
+                    },
+                    new Diet
+                    {
+                        Id = 3,
+                        Key = "interval_fasting",
+                        Name = "Intervallfasten"
+                    },
+                    new Diet
+                    {
+                        Id = 4,
+                        Key = "vegetarian_milk",
+                        Name = "Vegetarisch mit Milch, Ei, Fisch"
+                    },
+                    new Diet
+                    {
+                        Id = 5,
+                        Key = "lactose_free",
+                        Name = "Laktosefrei"
+                    },
+                    new Diet
+                    {
+                        Id = 6,
+                        Key = "pescetarian",
+                        Name = "Vegetarisch mit Fisch"
+                    },
+                    new Diet
+                    {
+                        Id = 7,
+                        Key = "low_fructose",
+                        Name = "Frustosearm"
+                    }
+                );
+
+                builder.Entity<Image>().HasData(
+                    new Image
+                    {
+                        Id = 1,
+                        Url = "https://myfooddoctormockcmsimgs.blob.core.windows.net/images/253f35f4-f3ac-425c-93ff-6edfdb62a12f.jpg"
+                    },
+                    new Image
+                    {
+                        Id = 2,
+                        Url = "https://myfooddoctormockcmsimgs.blob.core.windows.net/images/a5ec0b6f-d3cc-4a52-8283-2d7dba9a560c.jpg"
+                    }
+                );
+
+                builder.Entity<LexiconEntry>().HasData(
+                    new LexiconEntry
+                    {
+                        Id = 1,
+                        TitleShort = "Eiweiß",
+                        TitleLong = "Eiweiß",
+                        ImageId = 1,
+                        Text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+                    },
+                    new LexiconEntry
+                    {
+                        Id = 2,
+                        TitleShort = "Proteine",
+                        TitleLong = "Proteine",
+                        ImageId = 1,
+                        Text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+                    }
+                );
+
+                var hasher = new PasswordHasher<User>();
+                builder.Entity<User>().HasData(
+                    new User
+                    {
+                        Id = "3ee857ac-26ee-43d8-8f68-76f1ca7bfa9b",
+                        UserName = "test@appsfactory.de",
+                        NormalizedUserName = "TEST@APPSFACTORY.DE",
+                        Email = "test@appsfactory.de",
+                        NormalizedEmail = "TEST@APPSFACTORY.DE",
+                        EmailConfirmed = true,
+                        PasswordHash = hasher.HashPassword(null, "Wert123+"),
+                        SecurityStamp = string.Empty,
+                        InsuranceId = 1
+                    }
+                );
+
+                
+
+                var resourceName = "MyFoodDoc.Infrastructure.Persistence.Database.Seed.Ingredients.csv";
+                var assembly = Assembly.GetExecutingAssembly();
+
+                foreach (var x in assembly.GetManifestResourceNames())
+                {
+                    Console.WriteLine("1: " + x);
+                }
+
+                using var stream = assembly.GetManifestResourceStream(resourceName);
+                using var reader = new StreamReader(stream, Encoding.UTF8);
+                using var csv = new CsvReader(reader);
+
+                csv.Configuration.RegisterClassMap<IngredientsMap>();
+                csv.Configuration.BadDataFound = null;
+                csv.Configuration.Delimiter = ",";
+
+                var ingredients = csv.GetRecords<Ingredient>().Where(x => !x.ExternalKey.EndsWith("00000")).ToArray();
+                var id = 1;
+                foreach (var ingredient in ingredients)
+                {
+                    ingredient.Id = id++;
+                }
+                builder.Entity<Ingredient>().HasData(ingredients);
             }
-            builder.Entity<Ingredient>().HasData(ingredients);
         }
 
         class IngredientsMap : ClassMap<Ingredient>
