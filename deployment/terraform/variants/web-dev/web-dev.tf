f16a8bcc17dbc9a29c8919a4d21d56be3b94a133
Nikolai Shmatenkov <Nikolai.Shmatenkov@appsfactory.de>
2020-01-09 12:04:06 +0100

    [fix] one more try

--- deployment/terraform/variants/web-dev/web-dev.tf
+++ deployment/terraform/variants/web-dev/web-dev.tf
@@ -18,15 +18,16 @@ terraform {
 #######################################################
 # Remote state injection
 #######################################################
-#data "terraform_remote_state" "shared" {
-#  backend = "azurerm"
-#  config = {
-#    resource_group_name  = "mfd-terraform"
-#    storage_account_name = "mfdtfstate"
-#    container_name       = "terraform"
-#    key                  = "shared.terraform.tfstate"
-#  }
-#}
+data "terraform_remote_state" "shared" {
+  backend = "azurerm"
+  config = {
+    resource_group_name  = "mfd-terraform"
+    storage_account_name = "mfdtfstate"
+    container_name       = "terraform"
+    key                  = "shared.terraform.tfstate"
+    use_msi              = true
+  }
+}
 
 #######################################################
 # Module + Config
@@ -35,7 +36,7 @@ module "global" {
   source = "../../modules/web"
 
   projectname = "mfd"
-  stage       = "dev2"
+  stage       = "dev"
 
   apiserver_plantier     = "Basic"
   apiserver_plansize     = "B1"
@@ -49,10 +50,10 @@ module "global" {
   storageaccount_replication_type = "LRS"
   storageaccount_tier = "Standard"
 
-  keyvault_id            = "/subscriptions/d3359015-e21d-4560-8542-0eb0655d9f8f/resourceGroups/mfd-shared/providers/Microsoft.KeyVault/vaults/mfd-kv-shared" #data.terraform_remote_state.shared.outputs.keyvault_id
-  keyvault_name          = "mfd-kv-shared" #data.terraform_remote_state.shared.outputs.keyvault_name
+  keyvault_id            = data.terraform_remote_state.shared.outputs.keyvault_id
+  keyvault_name          = data.terraform_remote_state.shared.outputs.keyvault_name
 
-  containerregistry_url            = "mfdcontainers.azurecr.io" #data.terraform_remote_state.shared.outputs.containerregistry_url
-  containerregistry_admin_username = "mfdcontainers" #data.terraform_remote_state.shared.outputs.containerregistry_admin_username
-  containerregistry_admin_password = "PJYUZIkjkME8Ng/9O6qnB6I1X2I1L0Qi" #data.terraform_remote_state.shared.outputs.containerregistry_admin_password
+  containerregistry_url            = data.terraform_remote_state.shared.outputs.containerregistry_url
+  containerregistry_admin_username = data.terraform_remote_state.shared.outputs.containerregistry_admin_username
+  containerregistry_admin_password = data.terraform_remote_state.shared.outputs.containerregistry_admin_password
 }
