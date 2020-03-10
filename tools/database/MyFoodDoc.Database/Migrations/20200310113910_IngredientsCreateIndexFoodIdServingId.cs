e1788f307d084cbbad51fd355e6cb3f3c580367b
azverkov <alexey.zverkov@appsfactory.de>
2020-03-10 12:44:25 +0100

    Ingredients table. Unique index(FoodId, ServingId) added

---
+++ tools/database/MyFoodDoc.Database/Migrations/20200310113910_IngredientsCreateIndexFoodIdServingId.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyFoodDoc.Database.Migrations
+{
+    public partial class IngredientsCreateIndexFoodIdServingId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Ingredients_FoodId_ServingId",
+                schema: "Food",
+                table: "Ingredients",
+                columns: new[] { "FoodId", "ServingId" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Ingredients_FoodId_ServingId",
+                schema: "Food",
+                table: "Ingredients");
+        }
+    }
+}
