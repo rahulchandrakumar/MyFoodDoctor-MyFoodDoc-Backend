30f0eea26a9a100c2611e795294827e464d50699
azverkov <alexey.zverkov@appsfactory.de>
2020-07-14 13:49:41 +0200

    1. Check fat secret Ids before insert/update meals 2. 409 status on register if user already exists

---
+++ src/MyFoodDoc.App.Application/Exceptions/ConflictException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFoodDoc.App.Application.Exceptions
+{
+    public class ConflictException : ApplicationException
+    {
+        public ConflictException(string message)
+            : base(message)
+        {
+        }
+    }
+}
