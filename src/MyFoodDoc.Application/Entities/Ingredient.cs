e0bcaa613570bbf43a629966415bc8e43f9801da
azverkov <alexey.zverkov@appsfactory.de>
2020-08-21 12:17:58 +0200

    Database. Add external(Fat secret) nutritions to Ingredients table

--- src/MyFoodDoc.Application/Entities/Ingredient.cs
+++ src/MyFoodDoc.Application/Entities/Ingredient.cs
@@ -24,27 +24,51 @@ namespace MyFoodDoc.Application.Entities
 
         public decimal? Calories { get; set; }
 
+        public decimal CaloriesExternal { get; set; }
+
         public decimal? Carbohydrate { get; set; }
+
+        public decimal CarbohydrateExternal { get; set; }
 
         public decimal? Protein { get; set; }
 
+        public decimal ProteinExternal { get; set; }
+
         public decimal? Fat { get; set; }
+
+        public decimal FatExternal { get; set; }
 
         public decimal? SaturatedFat { get; set; }
 
+        public decimal SaturatedFatExternal { get; set; }
+
         public decimal? PolyunsaturatedFat { get; set; }
+
+        public decimal PolyunsaturatedFatExternal { get; set; }
 
         public decimal? MonounsaturatedFat { get; set; }
 
+        public decimal MonounsaturatedFatExternal { get; set; }
+
         public decimal? Cholesterol { get; set; }
+
+        public decimal CholesterolExternal { get; set; }
 
         public decimal? Sodium { get; set; }
 
+        public decimal SodiumExternal { get; set; }
+
         public decimal? Potassium { get; set; }
+
+        public decimal PotassiumExternal { get; set; }
 
         public decimal? Fiber { get; set; }
 
+        public decimal FiberExternal { get; set; }
+
         public decimal? Sugar { get; set; }
+
+        public decimal SugarExternal { get; set; }
 
         public decimal? Vegetables { get; set; }
 
