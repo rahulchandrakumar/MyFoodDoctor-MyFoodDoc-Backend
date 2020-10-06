dd3fda44eb8f85426c7319d5509fc473870df9e7
azverkov <alexey.zverkov@appsfactory.de>
2020-10-06 16:17:08 +0200

    Database. Lexicon categories

---
+++ src/MyFoodDoc.Application/Entities/LexiconCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MyFoodDoc.Application.Abstractions;
+
+namespace MyFoodDoc.Application.Entities
+{
+    public class LexiconCategory : AbstractAuditableEntity
+    {
+        public string Title { get; set; }
+
+        public int ImageId { get; set; }
+
+        public ICollection<LexiconEntry> Entries { get; set; }
+
+        public Image Image { get; set; }
+    }
+}
--- src/MyFoodDoc.Infrastructure/Persistence/Database/Configurations/LexiconEntryConfiguration.cs
+++ src/MyFoodDoc.Infrastructure/Persistence/Database/Configurations/LexiconEntryConfiguration.cs
@@ -17,6 +17,7 @@ namespace MyFoodDoc.Infrastructure.Persistence.Database.Configurations
             builder.Property(p => p.ImageId).IsRequired();
             builder.Property(p => p.Text).IsRequired();
 
+            builder.HasOne(x => x.Category).WithMany(x => x.Entries).HasForeignKey(x => x.CategoryId).OnDelete(DeleteBehavior.Cascade);
             builder.HasOne(x => x.Image).WithMany().HasForeignKey(x => x.ImageId);
         }
     }
