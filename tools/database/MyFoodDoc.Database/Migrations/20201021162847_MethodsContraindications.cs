b6f6a7a960b3845beeb748589595f3a8eee80b4a
azverkov <alexey.zverkov@appsfactory.de>
2020-10-21 18:30:23 +0200

    Database. MethodsContraindications

---
+++ tools/database/MyFoodDoc.Database/Migrations/20201021162847_MethodsContraindications.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyFoodDoc.Database.Migrations
+{
+    public partial class MethodsContraindications : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsContraindication",
+                schema: "System",
+                table: "MotivationMethods",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsContraindication",
+                schema: "System",
+                table: "IndicationMethods",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsContraindication",
+                schema: "System",
+                table: "DietMethods",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsContraindication",
+                schema: "System",
+                table: "MotivationMethods");
+
+            migrationBuilder.DropColumn(
+                name: "IsContraindication",
+                schema: "System",
+                table: "IndicationMethods");
+
+            migrationBuilder.DropColumn(
+                name: "IsContraindication",
+                schema: "System",
+                table: "DietMethods");
+        }
+    }
+}
