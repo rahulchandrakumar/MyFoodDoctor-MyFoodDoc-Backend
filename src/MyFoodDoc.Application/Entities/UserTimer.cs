9f0b624220ff0609fe925a056f573b066c9f6362
azverkov <alexey.zverkov@appsfactory.de>
2020-12-03 15:12:41 +0100

    Database. UserTimer

---
+++ src/MyFoodDoc.Application/Entities/UserTimer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFoodDoc.Application.Entities
+{
+    public class UserTimer
+    {
+        public string UserId { get; set; }
+
+        public DateTime ExpirationDate { get; set; }
+
+        public User User { get; set; }
+    }
+}
---
+++ src/MyFoodDoc.Infrastructure/Persistence/Database/Configurations/UserTimerConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyFoodDoc.Application.Entities;
+
+namespace MyFoodDoc.Infrastructure.Persistence.Database.Configurations
+{
+    public class UserTimerConfiguration : IEntityTypeConfiguration<UserTimer>
+    {
+        public void Configure(EntityTypeBuilder<UserTimer> builder)
+        {
+            builder.ToTable("UserTimer", "System");
+
+            builder.HasKey(x => x.UserId);
+
+            builder.Property(x => x.UserId).IsRequired().HasMaxLength(450);
+
+            builder.HasIndex(x => x.ExpirationDate);
+
+            builder.HasOne(x => x.User).WithMany().HasForeignKey(x => x.UserId).OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
