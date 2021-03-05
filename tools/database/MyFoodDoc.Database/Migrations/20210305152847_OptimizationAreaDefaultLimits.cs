7e21f1899c8988b98a748b891bb53d95e3c5d4c8
azverkov <alexey.zverkov@appsfactory.de>
2021-03-05 16:41:21 +0100

    API. Targets. Limits for protein optimization area

---
+++ tools/database/MyFoodDoc.Database/Migrations/20210305152847_OptimizationAreaDefaultLimits.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyFoodDoc.Database.Migrations
+{
+    public partial class OptimizationAreaDefaultLimits : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "System",
+                table: "OptimizationAreas",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "LineGraphLowerLimit", "LineGraphUpperLimit" },
+                values: new object[] { 0.8m, 1.5m });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "System",
+                table: "OptimizationAreas",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "LineGraphLowerLimit", "LineGraphUpperLimit" },
+                values: new object[] { null, null });
+        }
+    }
+}
