970e799cd23d944f945be91c704d05735884b2f3
Andrey Toropov <andrey.toropov@appsfactory.de>
2021-03-15 15:12:14 +0100

    API. AOK Validation

---
+++ MyFoodDoc.AokClient/Abstractions/IAokClient.cs
@@ -0,0 +1,11 @@
+﻿using MyFoodDoc.AokClient.Clients;
+using System;
+using System.Threading.Tasks;
+
+namespace MyFoodDoc.AokClient.Abstractions
+{
+    public interface IAokClient
+    {
+        Task<ValidateResponse> ValidateAsync(string insuranceNumber, DateTime birthday, string source = "myfooddock");
+    }
+}
---
+++ MyFoodDoc.AokClient/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyFoodDoc.AokClient.Abstractions;
+using MyFoodDoc.AokClient.Clients;
+using System;
+
+namespace MyFoodDoc.AokClient
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddSharedAokClient(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<AokClientOptions>(configuration.GetSection("Aok"));
+
+            var aokServiceUrl = configuration.GetValue<string>("AokClient:Address");
+
+            services.AddHttpClient<IAokClient, Clients.AokClient>(client =>
+            {
+                client.BaseAddress = new Uri(aokServiceUrl);
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+            });
+
+            return services;
+        }
+    }
+}
