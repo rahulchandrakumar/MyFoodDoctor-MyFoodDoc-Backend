8a4cd53ebf0967409f182f7691d0ab5bda49d0a3
Andrey Toropov <andrey.toropov@appsfactory.de>
2021-03-16 13:36:50 +0100

    API. AOK Client

--- src/MyFoodDoc.AokClient/Abstractions/IAokClient.cs
+++ src/MyFoodDoc.AokClient/Abstractions/IAokClient.cs
@@ -7,5 +7,7 @@ namespace MyFoodDoc.AokClient.Abstractions
     public interface IAokClient
     {
         Task<ValidateResponse> ValidateAsync(string insuranceNumber, DateTime birthday, string source = "myfooddock");
+        Task<RegisterResponse> Register(RegisterRequest request);
+        Task Delete(string token);
     }
 }
---
+++ src/MyFoodDoc.AokClient/Clients/RegisterRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MyFoodDoc.AokClient.Clients
+{
+    public class RegisterRequest
+    {
+        public string InsuranceNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Source { get; set; } = "myfooddock";
+    }
+}
